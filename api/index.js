9a7d2a1230ab9875cb81f7c553c5dd6bd693c2ae
NineNightdemomnight <42994065+NineNightdemomnight@users.noreply.github.com>
2024-09-21 20:31:57 +0700

    Delete api directory

--- api/index.js
+++
@@ -1,9 +0,0 @@
-import bypassFluxus from './bypassFluxus';
-
-export default async function handler(req, res) {
-  if (req.method === 'GET' && req.query.startUrl) {
-    return bypassFluxus(req, res);
-  }
-
-  res.status(404).json({ message: "Not Found" });
-}
