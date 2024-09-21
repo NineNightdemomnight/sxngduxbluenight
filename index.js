2d97e2571b0f6e20c4fad05f283fb00f42e9ece6
NineNightdemomnight <42994065+NineNightdemomnight@users.noreply.github.com>
2024-09-21 23:31:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 18012;
 
 // เส้นทางหลัก
 app.get('/', (req, res) => {
